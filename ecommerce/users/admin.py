1d3430ab79885ea69c3c0c6ead5808af2c3cac45
Ahmed Hassan <drahmedhassan.1990@gmail.com>
2025-03-30 07:55:08 +0200

    added products features

--- ecommerce/users/admin.py
+++ ecommerce/users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from users.models import CustomUser
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(CustomUser, UserAdmin)
